bd8e8ce8671efa75d2eaf80271fb62a85d666868
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2023-11-30 16:51:41 +0300

    AngularHydroteion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Signal, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template :`
+  <h1>{{Count()}}</h1>
+  <h2>Computed:{{CountPlus()}}</h2>
+  <br>
+  <button (click)="increment()">+</button>
+  <button (click)="decrement()">-</button>
+  <hr>
+  <ul>
+  <li *ngFor
+  ="let r of results">{{r}}
+  </li>
+  </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'example';
+  count=signal(0);
+  results=signal<string[]>([]);
+  countPlus=computed(()=>this.count()+);
+_=effect(()=>console.log(this.count()+"değişti"));
+  increment()
+  {
+    //
+  }
+  decrement(){
+   //
+  }
 }
